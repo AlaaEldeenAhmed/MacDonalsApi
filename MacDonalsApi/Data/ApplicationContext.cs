088b2a86a465085ddc005e30f91db35980759917
AlaaEldeen Ahmed <alaaahmed8383@gmail.com>
2021-07-02 09:38:48 +0400

    fourth commit

---
+++ MacDonalsApi/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using MacDonalsApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacDonalsApi.Data
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderDetail> orderDetails { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ShoppingCartItem> shoppingCartItems { get; set; }
+        public DbSet<User> users { get; set; }
+    }
+}
