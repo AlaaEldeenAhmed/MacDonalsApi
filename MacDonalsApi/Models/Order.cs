67b37578dd2c3d6892dbdebaddc8ae1bdb785a38
AlaaEldeen Ahmed <alaaahmed8383@gmail.com>
2021-07-02 09:09:32 +0400

    secound commit

---
+++ MacDonalsApi/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacDonalsApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public double OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool isOrderCompleted { get; set; }
+
+
+    }
+}
